5f09795f64d131f2fbb92f3653d8ba3e1f022b5e
Alexei <alexei.prudnikov@gmail.com>
2022-03-05 15:54:11 +0300

    Add Task 19

---
+++ Project01/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("=======Задача № 19=======");
+Console.WriteLine("Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом");
+
+bool IsPolindromicNumber(int number)
+{
+    number = Math.Abs(number);
+    string numStr = number.ToString();
+    for (int i = 0; i < (numStr.Length / 2); i++)
+    {
+        if (numStr[i] != numStr[numStr.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+try
+{
+    Console.Write("Введите пятизначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (((number >= 10000) & (number <= 99999))|((number <= -10000) & (number >= -99999)))
+    {
+        Console.Write("Число " + number);
+        if (!IsPolindromicNumber(number)) Console.Write(" НЕ");
+        Console.Write(" является полиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Число не является пятизначным.");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
